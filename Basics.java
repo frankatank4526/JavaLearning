10e55b921a1e2cd3c1b678e4c539625ac82d3711
frankatank4526 <frankatank1@gmail.com>
2021-11-26 19:55:05 -0800

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,19 @@
+
+public class Basics {
+
+	public static void main(String[] args){
+		// The main method executes the tasks of the class
+		System.out.println("My first Java program from scratch!");
+		//Reminder for Strings
+		String quotes = "\"Still valid with quotes\""; 
+		System.out.println(quotes);
+		
+		String backslash = "This is a backslash //";
+		System.out.println(backslash);
+		
+		String lineBreak = "Line 1 \nLine 2";
+		System.out.println(lineBreak);
+		
+		}
+
+}
