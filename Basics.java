1c211b855241ecbb952e0ffe06e92d02cf4a9f91
frankatank4526 <frankatank1@gmail.com>
2021-11-26 20:06:24 -0800

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -3,6 +3,13 @@ public class Basics {
 
 	public static void main(String[] args){
 		// The main method executes the tasks of the class
+		//Example variables in Java
+		String name = "Frank";
+		int age = 20;
+		double desiredSalary = 100000.00;
+		char gender = 'm';
+		boolean lookingForJob = true;
+		
 		System.out.println("My first Java program from scratch!");
 		//Reminder for Strings
 		String quotes = "\"Still valid with quotes\""; 
@@ -13,6 +20,8 @@ public class Basics {
 		
 		String lineBreak = "Line 1 \nLine 2";
 		System.out.println(lineBreak);
+		
+		
 		
 		}
 
