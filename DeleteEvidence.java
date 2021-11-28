f5a756a802d0c5cc452ef3dec8ade88c405e6d73
frankatank4526 <frankatank1@gmail.com>
2021-11-28 12:32:33 -0800

    If statements, loops, and more

---
+++ DeleteEvidence.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import static java.lang.System.out;
+import java.util.Scanner; 
+
+public class DeleteEvidence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File evidence = new File("cookedBooks.txt");
+		 Scanner keyboard = new Scanner(System.in);
+		 char reply;
+		 //do-while loop example 
+		 do {
+		 out.print("Delete evidence? (y/n) ");
+		 //findWithinHorizon Example, "." matches any character. More review
+		 //needed to understand findWithinHorizon and it's applications
+		 reply =
+		 keyboard.findWithinHorizon(".",0).charAt(0);
+		 } while (reply != 'y' && reply != 'n');
+		 if (reply == 'y') {
+		 out.println("Okay, here goes...");
+		 evidence.delete();
+		 out.println("The evidence has been deleted.");
+		 } else {
+		 out.println("Sorry, buddy. Just asking.");
+		 }
+		 keyboard.close();
+	}
+
+}
---
+++ GuessingGame.java
@@ -0,0 +1,28 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.util.Random;
+//static System.out imported, so only 'out.print' is necessary
+public class GuessingGame {
+//Review on if statements and computer input
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		 out.print("Enter an int from 1 to 10: ");
+		 int inputNumber = keyboard.nextInt();
+		 int randomNumber = new Random().nextInt(10) + 1;
+		 
+		 if (inputNumber == randomNumber) {
+		 out.println("**********");
+		 out.println("*You win.*");
+		 out.println("**********");
+		 } 
+		 else {
+		 out.println("You lose.");
+		 out.print("The random number was ");
+		 out.println(randomNumber + ".");
+		 }
+		 out.println("Thank you for playing.");
+		 keyboard.close();
+	}
+
+}
